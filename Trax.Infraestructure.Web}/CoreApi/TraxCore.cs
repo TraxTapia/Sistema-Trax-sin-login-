3f55e6a223ddd893a7352b9e01629f3867a21797
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2022-01-06 23:26:06 -0600

    Se crea el repositorio

---
+++ Trax.Infraestructure.Web}/CoreApi/TraxCore.cs
@@ -0,0 +1,44 @@
+﻿using RAX.Models.Web.Api.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trax.Infraestructure.Web_.ClienteApi;
+using TRAX.Models.Web.Api.OperationResult;
+using TRAX.Models.Web.Api.TraxApi.Request.RequestCajaAhorro;
+using TRAX.Models.Web.Api.TraxApi.Response;
+
+namespace Trax.Infraestructure.Web_.CoreApi
+{
+    public class TraxCore
+    {
+        private Logger _Logger;
+
+        public TraxCore(Logger Logger)
+        {
+            this._Logger = Logger;
+        }
+        public ObtenerAhorroByUsuarioReponseDTO ObtenerUsuario(ObtenerAhorroByUsuarioRequestDTO request, string EndPoint)
+        {
+            ObtenerAhorroByUsuarioReponseDTO _Response = new ObtenerAhorroByUsuarioReponseDTO();
+            try
+            {
+                ClientApiTrax _client = new ClientApiTrax(this._Logger);
+                _client.SetEndPoint(EndPoint);
+                _Response = _client.ObtenerUsuarioById(request);
+            }
+            catch (Exception ex)
+            {
+                this._Logger.LogError(ex);
+                _Response.Result.SetStatusCode(OperationResult.StatusCodesEnum.INTERNAL_SERVER_ERROR);
+                _Response.Result.AddException(ex);
+            }
+            return _Response;
+
+        }
+
+
+
+    }
+}
---
+++ UI.Trax.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UI.Trax.Web.MvcApplication" Language="C#" %>
